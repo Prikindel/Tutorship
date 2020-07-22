e5f407388cf1371d2ee6cd8198a714bd3a9cf995
Prikindel <kiryakovm96@gmail.com>
2020-07-22 12:55:23 +0300

    Added Register.kt in domain layer (Use Case)

---
+++ app/src/main/java/com/prike/tutorship/domain/account/Register.kt
@@ -0,0 +1,18 @@
+package com.prike.tutorship.domain.account
+
+import com.prike.tutorship.domain.interactor.UseCase
+import com.prike.tutorship.domain.type.Either
+import com.prike.tutorship.domain.type.None
+import com.prike.tutorship.domain.type.exception.Failure
+import javax.inject.Inject
+
+class Register @Inject constructor(
+    private val repository: AccountRepository
+) : UseCase<None, Register.Params>() {
+
+    override suspend fun run(params: Register.Params): Either<Failure, None> {
+        return repository.register(params.email, params.password)
+    }
+
+    data class Params(val email: String, val password: String)
+}
