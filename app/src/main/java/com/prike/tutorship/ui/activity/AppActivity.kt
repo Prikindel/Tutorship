12600c653dde9ea5490399d7c58e142496b80ca1
Prikindel <kiryakovm96@gmail.com>
2020-07-27 19:04:27 +0300

    Create app pages (timetable, chat, profile)

---
+++ app/src/main/java/com/prike/tutorship/ui/activity/AppActivity.kt
@@ -0,0 +1,9 @@
+package com.prike.tutorship.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prike.tutorship.R
+
+class AppActivity : BaseActivity() {
+    override val contentId = R.layout.activity_app
+}
---
+++ app/src/main/java/com/prike/tutorship/ui/fragment/TimetableFragment.kt
@@ -0,0 +1,7 @@
+package com.prike.tutorship.ui.fragment
+
+import com.prike.tutorship.R
+
+class TimetableFragment : BaseFragment() {
+    override val layoutId = R.layout.fragment_timetable
+}
