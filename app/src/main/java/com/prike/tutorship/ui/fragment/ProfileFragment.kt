1359deb697b93beb99900b18812d8cf9cce4513e
Prikindel <kiryakovm96@gmail.com>
2020-07-29 17:55:38 +0300

    Changed name toolbar app pages

--- app/src/main/java/com/prike/tutorship/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/prike/tutorship/ui/fragment/ProfileFragment.kt
@@ -4,4 +4,6 @@ import com.prike.tutorship.R
 
 class ProfileFragment : BaseFragment() {
     override val layoutId = R.layout.fragment_profile
+
+    override val titleToolbar = R.string.profile_toolbar
 }
