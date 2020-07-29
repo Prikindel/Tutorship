3799db66ed5776772c79de6dff67258325e9c9a5
Prikindel <kiryakovm96@gmail.com>
2020-07-29 10:10:05 +0300

    Added navigation activity app

--- app/src/main/java/com/prike/tutorship/ui/activity/AppActivity.kt
+++ app/src/main/java/com/prike/tutorship/ui/activity/AppActivity.kt
@@ -1,10 +1,9 @@
 package com.prike.tutorship.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.prike.tutorship.R
 import kotlinx.android.synthetic.main.activity_app.*
 
@@ -14,7 +13,6 @@ class AppActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //bottom_nav_view.setupWithNavController(findNavController(contentId))
-        //NavigationUI.setupWithNavController(bottom_nav_view, findNavController(R.layout.fragment_register))
+        NavigationUI.setupWithNavController(bottom_nav_view, findNavController(R.id.fragmentContainer))
     }
 }
