5ec77e69da1ed542b5ee3d97b22c5496a8830911
Prikindel <kiryakovm96@gmail.com>
2020-08-17 19:50:16 +0300

    Design register_info_fragment.xml

--- app/src/main/java/com/prike/tutorship/ui/sign/RegisterInfoFragment.kt
+++ app/src/main/java/com/prike/tutorship/ui/sign/RegisterInfoFragment.kt
@@ -9,18 +9,20 @@ import com.prike.tutorship.presenters.viewmodel.AccountViewModel
 import com.prike.tutorship.ui.App
 import com.prike.tutorship.ui.core.ext.onFailure
 import com.prike.tutorship.ui.fragment.BaseFragment
+import kotlinx.android.synthetic.main.register_info_fragment.*
 import kotlinx.android.synthetic.main.register_name_fragment.*
+import kotlinx.android.synthetic.main.register_name_fragment.btnLogin
 
 class RegisterInfoFragment : SignFragmentBase(R.layout.register_info_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnNextStep.setOnClickListener {
+        btnMan.setOnClickListener {
             showMessage("Здесь будет переход на следующий шаг регистрации")
-            val builder = MaterialDatePicker.Builder.datePicker()
-            val picker = builder.build()
-            picker.showNow(this.parentFragmentManager, "123")
+        }
+        btnWoman.setOnClickListener {
+            showMessage("Здесь будет переход на следующий шаг регистрации")
         }
 
         btnLogin.setOnClickListener {
