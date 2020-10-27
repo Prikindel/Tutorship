e07026b41a37bd9bab1e2332014ce74ee477f835
Prikindel <kiryakovm96@gmail.com>
2020-10-27 18:32:16 +0300

    Базовый адаптер изменен для работы с передаваемым типом

--- app/src/main/java/com/prike/tutorship/ui/adapter/BaseAdapterForRecyclerView.kt
+++ app/src/main/java/com/prike/tutorship/ui/adapter/BaseAdapterForRecyclerView.kt
@@ -7,11 +7,12 @@ import androidx.recyclerview.widget.RecyclerView
  * Абстрактрный, базовый класс адаптера для списков, содержащий базовые методы добавления, получения элементов.
  * А также базовый ViewHolder
  *
+ * @param Type тип элементов списка
  * @param VH ViewHolder
  * @property items список элементов
  */
-abstract class BaseAdapterForRecyclerView<VH : BaseAdapterForRecyclerView.BaseViewHolder> (
-    private val items: MutableList<Any>
+abstract class BaseAdapterForRecyclerView<Type : Any, VH : BaseAdapterForRecyclerView.BaseViewHolder<Type>> (
+    private val items: MutableList<Type>
 ) : RecyclerView.Adapter<VH>() {
 
     /**
@@ -42,21 +43,24 @@ abstract class BaseAdapterForRecyclerView<VH : BaseAdapterForRecyclerView.BaseVi
      *
      * @param newitem элемент
      */
-    fun add(newitem: Any) = items.add(newitem)
+    fun add(newitem: Type) = items.add(newitem)
 
     /**
      * Добавление списка элементов
      *
      * @param newItems список элементов
      */
-    fun add(newItems: List<Any>) = items.addAll(newItems)
+    fun add(newItems: List<Type>) = items.addAll(newItems)
 
     /**
      * Абстрактный класс ViewHolder
      *
+     * @param Type тип элемента
      * @property view
      */
-    abstract class BaseViewHolder(protected val view: View) : RecyclerView.ViewHolder(view) {
-        abstract fun bind(item: Any)
+    abstract class BaseViewHolder<Type : Any> (
+        protected val view: View
+    ) : RecyclerView.ViewHolder(view) {
+        abstract fun bind(item: Type)
     }
 }
